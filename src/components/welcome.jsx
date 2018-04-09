862942e32411ec6f3d3666ed667b3f614ca0c55b
Sergio <uberbosser@gmail.com>
2018-04-09 06:21:57 -0400

    Improve animation

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -8,9 +8,7 @@ class Welcome extends Component {
     render() {
         return (
             <div id="welcome">
-                <div id="heart-container">
-                    <img id="heart" src={heartImage} alt="heart" />
-                </div>
+                <img id="heart" src={heartImage} alt="heart" />
                 <h1>{this.props.children}</h1>
                 <h3><a class="text-white" href="https://github.com/UberBosser/GoTemplate">GoTemplate</a></h3>
             </div>
